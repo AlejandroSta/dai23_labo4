298668a5075f979f55d09d55a4257c7b9c9b4910
AlejandroSta <alejandro.stadlin@heig-vd.ch>
2023-11-09 10:44:33 +0100

    Initial commit

---
+++ src/main/java/labo4/gonin_stadlin/dai23_labo4/HelloController.java
@@ -0,0 +1,14 @@
+package labo4.gonin_stadlin.dai23_labo4;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module labo4.gonin_stadlin.dai23_labo4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens labo4.gonin_stadlin.dai23_labo4 to javafx.fxml;
+    exports labo4.gonin_stadlin.dai23_labo4;
+}
